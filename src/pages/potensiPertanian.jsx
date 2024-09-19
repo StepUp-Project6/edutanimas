f0f652d30eb79272f75a30d9f3131e50cbd6ea8d
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-19 08:03:51 +0700

    menambahkan judul pada page potensi pertanian

--- src/pages/potensiPertanian.jsx
+++ src/pages/potensiPertanian.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Fragments/Navbar";
+import HeadSection from "../components/Elements/HeadSection";
 
 const PotensiPertanian = () => {
   return (
-    <div>PotensiPertanian</div>
-  )
-}
+    <div className="font-mulish min-h-screen flex flex-col">
+      <Navbar />
+      <main className="w-full mt-[70px] border border-black">
+        <HeadSection
+          content={[
+            { title: "Hasil " },
+            { title: "Pertanian", classname: "text-primary" },
+          ]}
+        />
+      </main>
+    </div>
+  );
+};
 
-export default PotensiPertanian
+export default PotensiPertanian;
