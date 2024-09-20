25465349cdabff1dec404508fe4857b7e037003f
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-20 11:13:11 +0700

    membuat table dan data produksi holtikultura

--- src/pages/potensiPertanian.jsx
+++ src/pages/potensiPertanian.jsx
@@ -1,18 +1,59 @@
 import React from "react";
 import Navbar from "../components/Fragments/Navbar";
 import HeadSection from "../components/Elements/HeadSection";
+import TextParagraph from "../components/Elements/TextParagraph";
+import { DataHoltikultura } from "../utils/DataHoltikultura";
+import TableProduction from "../components/Fragments/TableProduction";
 
 const PotensiPertanian = () => {
   return (
     <div className="font-mulish min-h-screen flex flex-col">
       <Navbar />
-      <main className="w-full mt-[70px] border border-black">
+      <main className="w-full mt-[70px] border border-black pb-40">
         <HeadSection
           content={[
             { title: "Hasil " },
             { title: "Pertanian", classname: "text-primary" },
           ]}
         />
+        <TextParagraph
+          customClassname="text-center mt-3 px-14"
+          content="Potensi Hasil Pertanian di Kecamatan Bringin disajikan dalam tabel di bawah ini."
+        />
+        {/* SECTION PRODUKSI HOLTIKULTURA*/}
+        <section className="relative w-full border border-black pt-[510px] mt-14 h-[950px]">
+          <img
+            src="images/element-bg-3.png"
+            alt="bg"
+            className="absolute bottom-0 w-full -z-50"
+          />
+          {/* IMAGE ABSOLUTE 1 : PRODUKSI HOLTIKULTURA */}
+
+          {/* TABEL PRODUKSI KOLTIKULTURA*/}
+          <div className="px-6">
+            <h2 className="px-10 leading-[16px]">
+              <HeadSection
+                content={[
+                  {
+                    title: "",
+                    className: "font-semibold text-[14px]",
+                  },
+                  {
+                    title: "Tabel Produksi Holtikultura di ",
+                    classname:
+                      "font-semibold text-[14px] text-black md:text-[36px] lg:text-[64px]",
+                  },
+                  {
+                    title: "Kecamatan Bringin ",
+                    classname:
+                      "text-primary font-semibold text-[14px] md:text-[36px] md:top-0 lg:text-[64px]",
+                  },
+                ]}
+              />
+            </h2>
+            <TableProduction data={DataHoltikultura} />
+          </div>
+        </section>
       </main>
     </div>
   );
---
+++ src/utils/DataHoltikultura.js
@@ -0,0 +1,56 @@
+export const DataHoltikultura = [
+    {
+        id: 1,
+        title: "Lombok Besar",
+        quantity: 1906,
+        image: "images/holtikultura/lombok-besar.png",   
+    },
+    {
+        id: 2,
+        title: "Lombok Rawit",
+        quantity: 999,
+        image: "images/holtikultura/lombok-rawit.png",   
+    },
+    {
+        id: 3,
+        title: "Tomat",
+        quantity: 95,
+        image: "images/holtikultura/tomat.png",   
+    },
+    {
+        id: 4,
+        title: "Kacang Panjang",
+        quantity: 696,
+        image: "images/holtikultura/kacang-panjang.png",   
+    },
+    {
+        id: 5,
+        title: "Terong",
+        quantity: 744,
+        image: "images/holtikultura/terong.png",   
+    },
+    {
+        id: 6,
+        title: "Labu Siam",
+        quantity: 60,
+        image: "images/holtikultura/labu-siam.png",   
+    },
+    {
+        id: 7,
+        title: "Bayam",
+        quantity: 216,
+        image: "images/holtikultura/bayam.png",   
+    },
+    {
+        id: 8,
+        title: "Petai",
+        quantity: 11420,
+        image: "images/holtikultura/patai.png",   
+    },
+    {
+        id: 9,
+        title: "Kangkung",
+        quantity: 270,
+        image: "images/holtikultura/kangkung.png",   
+    },
+]
