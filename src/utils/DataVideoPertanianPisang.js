30f3aa460c22bfb07e077abb2752d143fa1f4d8d
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-09-23 08:56:16 +0700

    membuat data untuk video pertanian cabai, padi, dan pisang

---
+++ src/utils/DataVideoPertanianPisang.js
@@ -0,0 +1,14 @@
+export const DataVideoPertanianPisang = [
+  {
+    id: 1,
+    title: "BEGINI PROSES BERKEBUN PISANG SECARA MODERN DI NEGERA MAJU",
+    image: "images/imageVideo/pertanian-pisang-1.png",
+    link: "https://youtu.be/g5Snnpgafec?si=gF5BXcWcLv5RxNym",
+  },
+  {
+    id: 2,
+    title: "Budidaya pisang cavendish modern",
+    image: "images/imageVideo/pertanian-pisang-2.png",
+    link: "https://youtu.be/g5Snnpgafec?si=EfyYOuYPe2CXkjx8",
+  },
+];
